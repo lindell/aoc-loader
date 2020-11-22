a421d2c564b83d9cc7ae58e4febba7e8217dc2d4
Gustav Utterheim <gustav@utterheim.com>
2020-11-22 14:09:16 +0100

    style: update indent size

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: './tsconfig.eslint.json',
-  },
-  env: {
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: './tsconfig.eslint.json',
+    },
+    env: {
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier',
+        'prettier/@typescript-eslint',
+    ],
 };
--- index.ts
+++ index.ts
@@ -7,43 +7,43 @@ import crypto from 'crypto';
 const { readFile, writeFile } = promises;
 
 async function getInput(
-  year: number,
-  day: number,
-  session = process.env.AOC_SESSION,
+    year: number,
+    day: number,
+    session = process.env.AOC_SESSION,
 ): Promise<string | undefined> {
-  if (session === undefined) {
-    return Promise.reject(
-      new Error('No session provided or exist as the AOC_SESSION environment variable'),
-    );
-  }
+    if (session === undefined) {
+        return Promise.reject(
+            new Error('No session provided or exist as the AOC_SESSION environment variable'),
+        );
+    }
 
-  const hash = crypto.createHash('sha256');
-  hash.update(session);
-  const hashedSession = hash.digest('hex');
+    const hash = crypto.createHash('sha256');
+    hash.update(session);
+    const hashedSession = hash.digest('hex');
 
-  const filename = `aoc-${year}-${day}-${hashedSession}`;
-  const tempPath = path.join(os.tmpdir(), filename);
+    const filename = `aoc-${year}-${day}-${hashedSession}`;
+    const tempPath = path.join(os.tmpdir(), filename);
 
-  let ret: string | undefined;
-  try {
-    const input = await readFile(tempPath)
-      .then((buffer) => buffer.toString('utf8'))
-      .catch(() => {
-        return got(`https://adventofcode.com/${year}/day/${day}/input`, {
-          headers: {
-            Cookie: `session=${session}`,
-          },
-          resolveBodyOnly: true,
-        });
-      });
+    let ret: string | undefined;
+    try {
+        const input = await readFile(tempPath)
+            .then((buffer) => buffer.toString('utf8'))
+            .catch(() => {
+                return got(`https://adventofcode.com/${year}/day/${day}/input`, {
+                    headers: {
+                        Cookie: `session=${session}`,
+                    },
+                    resolveBodyOnly: true,
+                });
+            });
 
-    ret = input.replace(/\n$/, '');
-    await writeFile(tempPath, ret);
-  } catch (_error) {
-    // Don't do anything if it fails
-  }
+        ret = input.replace(/\n$/, '');
+        await writeFile(tempPath, ret);
+    } catch (_error) {
+        // Don't do anything if it fails
+    }
 
-  return ret;
+    return ret;
 }
 
 export default getInput;
