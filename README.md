158352e489c31d00487b6f78d47be938f3d6f417
Gustav Utterheim <gustav@utterheim.com>
2020-11-22 16:46:29 +0100

    feat: convert to typescript (#1)

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-Advent of Code Input Loader
-----
+# Advent of Code Input Loader
+
 Loads [Advent of Code](http://adventofcode.com/) input data for a specified day and year.
 
-Install
-----
+## Install
+
 ```sh
 npm install aoc-loader
 ```
 
-Use
-----
+## Use
+
 ```javascript
 const aocLoader = require('aoc-loader');
 
-aocLoader(year, day, sessionCookie).then(data => {
+aocLoader(year, day, sessionCookie).then((data) => {
     console.log(data);
 });
 ```
 
-If no `sessionCookie` is provided, aoc-loander will try to get i from the `AOC_SESSION` environment variable. This is recomended the recomended way of using this package.
+If no `sessionCookie` is provided, aoc-loader will try to get i from the `AOC_SESSION` environment variable. This is recommended the recommended way of using this package.
 
-Get the session cookie
-----
+## Get the session cookie
+
 First sign in to advent of code. Then you can open the developer options of your browser and you should be able to find the session set as a cookie.
 In Chrome, you will find it under `Application -> Cookies -> https://adventofcode.com`
---
+++ index.ts
@@ -0,0 +1,46 @@
+import got from 'got';
+import os from 'os';
+import path from 'path';
+import { promises } from 'fs';
+import crypto from 'crypto';
+
+const { readFile, writeFile } = promises;
+
+async function getInput(year: number, day: number, session = process.env.AOC_SESSION): Promise<string | undefined> {
+    if (session === undefined) {
+        throw new Error('No session provided or exist as the AOC_SESSION environment variable');
+    }
+
+    const hash = crypto.createHash('sha256');
+    hash.update(session);
+    const hashedSession = hash.digest('hex');
+
+    const filename = `aoc-${year}-${day}-${hashedSession}`;
+    const tempPath = path.join(os.tmpdir(), filename);
+
+    let ret: string | undefined;
+    try {
+        const input = await readFile(tempPath)
+            .then((buffer) => buffer.toString('utf8'))
+            .catch(() => {
+                return got(`https://adventofcode.com/${year}/day/${day}/input`, {
+                    headers: {
+                        Cookie: `session=${session}`,
+                    },
+                    resolveBodyOnly: true,
+                });
+            });
+
+        ret = input.replace(/\n$/, '');
+        await writeFile(tempPath, ret);
+    } catch (_error) {
+        // Don't do anything if it fails
+    }
+
+    return ret;
+}
+
+export default getInput;
+
+// For CommonJS default export support
+module.exports = getInput;
