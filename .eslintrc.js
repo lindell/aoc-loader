2a674f70e4f74280a0097ab6a9bcd6707fa447c1
Gustav Utterheim <gustav@utterheim.com>
2020-12-04 18:45:13 +0100

    test: add unit test

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,24 @@ module.exports = {
     },
     env: {
         node: true,
+        jest: true,
     },
     extends: [
         'airbnb-base',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:import/typescript',
+        'plugin:jest/recommended',
         'prettier',
         'prettier/@typescript-eslint',
     ],
+    rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: ['**/*.spec.ts'] }],
+        'import/extensions': [
+            'error',
+            {
+                ts: 'never',
+            },
+        ],
+    },
 };
